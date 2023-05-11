ff61a156f414852eeb1160cffa5e5e8a880b3f77
Oh jou yeon <hyperclutter@gmail.com>
2023-05-11 12:32:26 +0900

    인식 오류 수정

---
+++ src/main/java/BookMap/PentaRim/User/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package BookMap.PentaRim.User;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getPassword() { return user.getPassword(); }
+
+    @Override
+    public String getUsername() { return user.getUsername();}
+
+    //계정 만료 여부 true:만료 안 됨 false:만료됨
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //계정 잠김 여부 true안잠김 false잠김
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //비번 만료 여부 true 만료 안 됨 false 만료
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //사용자 활성화 여부 true 만료 안됨 false만료됨
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    //유저 권한 목록
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collectors = new ArrayList<>();
+
+        collectors.add(()-> "ROLE_"+user.getRole());
+
+        return collectors;
+    }
+
+}
--- src/main/java/BookMap/PentaRim/User/SecurityConfig.java
+++ src/main/java/BookMap/PentaRim/User/SecurityConfig.java
@@ -1,7 +1,7 @@
 package BookMap.PentaRim.User;
 
 
-import lombok.Builder;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +11,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static BookMap.PentaRim.User.Role.USER;
 
 @RequiredArgsConstructor
 @Configuration
@@ -23,11 +24,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableMethodSecurity
 public class SecurityConfig  {
 
-    private final UserDetailsService myUserDetailsService;
 
-    private final AuthenticationFailureHandler customFailureHandler;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final CustomAuthFailureHandler customFailureHandler;
 
     private final CustomOAuth2UserService customOAuth2UserService;
+
 
     @Bean
     public BCryptPasswordEncoder Encoder() {
@@ -35,15 +38,27 @@ public class SecurityConfig  {
     }
 
     @Bean
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(Encoder())
+                .withUser("user").password("secret123").roles("USER");
+    }
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     //시큐리티 암호화 확인
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService).passwordEncoder(Encoder());
+    public InMemoryUserDetailsManager userDetailsManager() {
+        CustomUserDetails user = (User) User.builder()
+                .username("user")
+                .password("password")
+                .role(USER)
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
