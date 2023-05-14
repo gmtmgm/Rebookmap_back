8e10c6160d455ea99ff87ec12a3e92b2cce03abc
yubin <ydbqls13@naver.com>
2023-05-14 23:05:02 +0900

    레포지토리 추가, 개인 책 저장, 수정, 삭제 구현

--- src/main/java/BookMap/PentaRim/Book/SearchListBooks.java
+++ src/main/java/BookMap/PentaRim/Book/SearchListBooks.java
@@ -1,6 +1,5 @@
 package BookMap.PentaRim.Book;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
