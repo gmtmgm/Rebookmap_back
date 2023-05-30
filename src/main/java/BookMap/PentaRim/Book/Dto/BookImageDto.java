01809dd03336e04d417ea97573c4ae5dfec174d8
yubin <ydbqls13@naver.com>
2023-05-30 16:00:51 +0900

    main 페이지 dto에 isbn 추가

--- src/main/java/BookMap/PentaRim/Book/Dto/BookImageDto.java
+++ src/main/java/BookMap/PentaRim/Book/Dto/BookImageDto.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 @Getter
 public class BookImageDto {
     private Long id;
+    private String isbn;
     private String image;
 
     public BookImageDto(Book book){
         this.id = book.getId();
+        this.isbn = book.getIsbn();
         this.image = book.getImage();
     }
 }
