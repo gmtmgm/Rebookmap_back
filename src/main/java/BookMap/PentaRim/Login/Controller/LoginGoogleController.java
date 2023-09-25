9261709f3af8166444f47a8f71f101e465034643
Oh jou yeon <hyperclutter@gmail.com>
2023-09-25 19:18:52 +0900

    logingooglecontroller 추가

---
+++ src/main/java/BookMap/PentaRim/Login/Controller/LoginGoogleController.java
@@ -0,0 +1,65 @@
+package BookMap.PentaRim.Login.Controller;
+import BookMap.PentaRim.User.Role;
+import BookMap.PentaRim.User.model.User;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import static BookMap.PentaRim.User.Auth.Filter.JwtProperties.SECRET;
+
+@Slf4j
+@RestController
+public class LoginGoogleController {
+
+
+
+    @PostMapping("/login/google")
+    public static void loginGoogle(@RequestParam("idToken")String idTokenString) throws GeneralSecurityException, IOException {
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                // Specify the CLIENT_ID of the app that accesses the backend:
+                .setAudience(Collections.singletonList("205578501902-2g0aj7mkvmtbpqdcjqt86nhc64euj7l0.apps.googleusercontent.com"))
+                .build();
+
+
+        // (Receive idTokenString by HTTPS POST)
+
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+        if (idToken != null) {
+            Payload payload = idToken.getPayload();
+
+            // Print user identifier
+            String userId = payload.getSubject();
+            System.out.println("User ID: " + userId);
+
+            // Get profile information from payload
+            String email = payload.getEmail();
+            boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
+            String name = (String) payload.get("name");
+            String pictureUrl = (String) payload.get("picture");
+            String locale = (String) payload.get("locale");
+            String familyName = (String) payload.get("family_name");
+            String givenName = (String) payload.get("given_name");
+
+
+
+            // Use or store profile information
+
+
+            // ...
+
+        } else {
+            System.out.println("Invalid ID token.");
+        }
+    }
+    }
