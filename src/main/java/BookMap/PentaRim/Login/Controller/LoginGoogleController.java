0882b1422685c8fbf659825457aeef01f06dada9
Oh jou yeon <hyperclutter@gmail.com>
2023-10-09 12:24:45 +0900

    세션 수정 2

--- src/main/java/BookMap/PentaRim/Login/Controller/LoginGoogleController.java
+++ src/main/java/BookMap/PentaRim/Login/Controller/LoginGoogleController.java
@@ -89,10 +89,7 @@ public class LoginGoogleController {
 
               log.info("user : " + user);
 
-              if(sessionRepository.findByIndexNameAndIndexValue(SessionConst.EMAIL, email) != null) {
-                  return sessionRepository.findByIndexNameAndIndexValue(SessionConst.EMAIL, email).keySet().toString();
 
-              }
 
               //로그인 성공 처리
               //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
@@ -126,19 +123,14 @@ public class LoginGoogleController {
 
           } else {
 
-              if(sessionRepository.findByIndexNameAndIndexValue(SessionConst.EMAIL, email) != null) {
-                  return sessionRepository.findByIndexNameAndIndexValue(SessionConst.EMAIL, email).keySet().toString();
 
-              }
 
 
               //로그인 성공 처리
               //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
-              HttpSession session =request.getSession();
+              HttpSession session = (HttpSession) sessionRepository.findByIndexNameAndIndexValue(SessionConst.EMAIL,email).values();
               //세션에 로그인 회원 정보 보관
-              session.setAttribute(SessionConst.EMAIL, email);
 
-              session.setMaxInactiveInterval(-1);
 
               /*
 
